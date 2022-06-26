dccf84d19189f35376969eb70f65af57b7b99d28
lemokami <jesswinw@gmail.com>
2022-06-26 21:02:40 +0530

    refactor: removed created and updated at's and used default timestamps

--- models/PostModel.js
+++ models/PostModel.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
-const PostModel = new mongoose.Schema({
-  hash: { type: String, required: true },
-  path: { type: String, required: true },
-  likes: { type: Number, default: 0 },
-  shareable: { type: Boolean, required: true },
-  sharers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  owner: { type: Schema.Types.ObjectId, ref: 'User' },
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now },
-});
+const PostModel = new mongoose.Schema(
+  {
+    hash: { type: String, required: true },
+    path: { type: String, required: true },
+    likes: { type: Number, default: 0 },
+    shareable: { type: Boolean, required: true },
+    sharers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    owner: { type: Schema.Types.ObjectId, ref: 'User' },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Post = mongoose.model('Post', PostModel);
 
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 
-const UserModel = new mongoose.Schema({
-  key: { type: String, required: true },
-  name: { type: String },
-  profile_img: { type: String },
-  age: { type: Number },
-  completed_profile: { type: Boolean, default: false },
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date },
-});
+const UserModel = new mongoose.Schema(
+  {
+    key: { type: String, required: true },
+    name: { type: String },
+    profile_img: { type: String },
+    age: { type: Number },
+    completed_profile: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model('User', UserModel);
 

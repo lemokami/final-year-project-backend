c5e9426cdf21b6c36a3139dd296cac0082d4aa68
lemokami <jesswinw@gmail.com>
2022-06-28 02:41:50 +0530

    refactor: request handling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createRequire } from "module";
+import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 const cors = require('cors');
@@ -13,7 +13,6 @@ import { fileURLToPath } from 'url';
 import { create, urlSource } from 'ipfs-http-client';
 require('dotenv').config();
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -22,13 +21,13 @@ const User = require('./models/UserModel.cjs');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-express.static('public');
+app.use('/public', express.static('public'));
 
 async function ipfsClient() {
-  const ipfs = await create(new URL('http://127.0.0.1:5001'))
+  const ipfs = await create(new URL('http://127.0.0.1:5001'));
   return ipfs;
 }
 
@@ -39,7 +38,8 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     if (file) {
-      const uniqueSuffix = Date.now() + '-' + uuid.v4() + path.extname(file.originalname);
+      const uniqueSuffix =
+        Date.now() + '-' + uuid.v4() + path.extname(file.originalname);
       // const uniqueSuffix = path.extname(file.originalname);
       cb(null, file.fieldname + '-' + uniqueSuffix);
       // cb(null, "myImage" + uniqueSuffix);
@@ -52,16 +52,15 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // user signin
-app.get('/signin', async (req, res) => {
+app.post('/signin', async (req, res) => {
   try {
-    var user = await User.find({ key: req.body.key });
-    if (Object.entries(user).length != 0) {
-      console.log("User exists");
+    const user = await User.findOne({ key: req.body.key });
+    if (user) {
       res.status(200).json(user);
     } else {
-      var newUser = User.create({ 
+      const newUser = await User.create({
         key: req.body.key,
-        name: req.body.name
+        name: req.body.name,
       });
       res.status(200).json(newUser);
     }
@@ -75,10 +74,16 @@ app.patch('/user', upload.single('profile_img'), async (req, res) => {
   try {
     let data = { ...req.body, completed_profile: true };
     if (req.file) {
-      data = { ...req.body, completed_profile: true, file: req.file };
+      data = {
+        ...req.body,
+        profile_img: req.file.path,
+        completed_profile: true,
+      };
     }
 
-    const user = await User.updateOne({ key: req.body.key });
+    await User.findOneAndUpdate({ key: req.body.key }, data);
+    const user = await User.findOne({ key: req.body.key });
+
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: 'Error updating profile', error: error });
@@ -87,32 +92,35 @@ app.patch('/user', upload.single('profile_img'), async (req, res) => {
 
 // getting posts
 app.get('/posts', async (req, res) => {
-  const posts = await Post.find(null, null, { sort: { created_at: 1 } });
+  const posts = await Post.find(null, null, { sort: { created_at: -1 } });
 
   res.status(200).json(posts);
 });
 
 //creating posts
-app.post('/post', upload.single('file'), (req, res) => {
+app.post('/post', upload.single('file'), async (req, res) => {
   try {
     let metadata;
+    let hashes;
     var imagePath = __dirname + '/public/uploads/' + req.file.filename;
     new Exif({ image: imagePath }, async function (error, exifData) {
       if (error) {
         console.log('(try) Error: ' + error.message);
-        res.sendStatus(400);
-      }
-      else {
-        //sending ipfs and getting hash
+        res.status(400).send({ message: 'Exif parsing issue' });
+      } else {
         metadata = exifData;
+        //sending ipfs and getting hash
         const file = fs.readFileSync(imagePath);
         let imageFileName = req.file.filename;
-        let trimmedFileName = imageFileName.substring(0, imageFileName.length - 4);
+        let trimmedFileName = imageFileName.substring(
+          0,
+          imageFileName.length - 4
+        );
 
         let ipfs = await ipfsClient();
         let resultImage = await ipfs.add({
           path: imageFileName,
-          content: file
+          content: file,
         });
         console.log(resultImage);
 
@@ -121,27 +129,36 @@ app.post('/post', upload.single('file'), (req, res) => {
         metadata.walletid = req.body.key;
 
         let resultMetadata = await ipfs.add({
-          path: trimmedFileName + ".json",
-          content: JSON.stringify(metadata)
+          path: trimmedFileName + '.json',
+          content: JSON.stringify(metadata),
         });
         console.log(resultMetadata);
 
-        var result = {
-          "ipfsImageHash": ipfsHash,
-          "ipfsMetaDataHash": resultMetadata.cid.toString() 
+        hashes = {
+          metaContentHash: ipfsHash,
+          metaHash: resultMetadata.cid.toString(),
         };
+        // checking if post exists
+        const existingPost = await Post.findOne(hashes);
+        console.log(existingPost);
+        if (existingPost) {
+          res.status(409).json({ message: 'Content already exists' });
+        } else {
+          //creating file
+          const post = await Post.create({
+            path: req.file.path,
+            shareable: req.body.shareable,
+            metaHash: hashes.metaHash,
+            metaContentHash: hashes.metaContentHash,
+            owner: req.body.key,
+          });
 
-        res.status(200).json(result);
+          res.status(200).send(post);
+        }
       }
     });
-    // TODO: send to ipfs and get hash
-    // TODO: create a smartcontract from the hash
-    // TODO: create the post
-
-    // res.sendStatus(200);
   } catch (error) {
-    console.log('(catch) Error: ' + error.message);
-    res.sendStatus(400);
+    res.status(400).send(error.message);
   }
 });
 
@@ -156,10 +173,19 @@ app.post('/like/:id', async (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(
-    () => {console.log("db connected")},
-    err => {console.log("error occured will connecting to db" + err)}
-  );
-  
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(
+      () => {
+        console.log('db connected');
+      },
+      (err) => {
+        console.log('error occured will connecting to db' + err);
+      }
+    );
+
   console.log('listening on port ' + process.env.PORT);
 });
